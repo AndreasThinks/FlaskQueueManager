e30eb92386ba3b0d97847f97e9cee22fed0c9021
Andreas <andrea.varotsis@gmail.com>
2014-03-18 12:07:34 -0700

    First Milestone - AddTask form implemented

--- app/forms.py
+++ app/forms.py
@@ -6,5 +6,5 @@ class LoginForm(Form):
     openid = TextField('openid', validators = [Required()])
     remember_me = BooleanField('remember_me', default = False)
 
-class addTask(Form):
+class AddTask(Form):
     name = TextField('name', validators = [Required()])
--- app/views.py
+++ app/views.py
@@ -1,13 +1,17 @@
 from flask import render_template
 from app import app
+from forms import AddTask
+
 
 
 @app.route('/')
-@app.route('/index')
+@app.route('/index', methods = ['GET', 'POST'])
 def index():
+    form = AddTask()
     user = { 'nickname': 'Dan'}  # fake user
     tasks = {1: 'passport', 2: 'eat', 3: 'Amazing'}
     return render_template("index.html",
         title = 'Home',
         user = user,
+        form = form,
         tasks = tasks)
