af7eed2845bc4763800b830e13f391053cc473b8
Andreas <andrea.varotsis@gmail.com>
2014-03-21 16:12:32 -0700

    Milestone Two - adds and reads from DB, still very broken.

---
+++ app/models.py
@@ -0,0 +1,21 @@
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.String(7), primary_key = True)
+    email = db.Column(db.String(120), unique = True)
+    password = db.Column(db.String(10))
+    #tasks = db.relationship('Task', backref = 'creator_id', lazy = 'dynamic')
+
+    def __repr__(self):
+        return '<User %r>' % (self.id)
+
+    def get_id(self):
+        return unicode(self.id)
+
+class Task(db.Model):
+    number = db.Column(db.String(7), primary_key = True)
+    creator = db.Column(db.String(120), db.ForeignKey('user.id'))
+    time = db.Column(db.SmallInteger)
+
+    def __repr__(self):
+        return '<Task %r>' % (self.number)
--- app/views.py
+++ app/views.py
@@ -1,17 +1,48 @@
-from flask import render_template
-from app import app
-from forms import AddTask
+from flask import render_template, session, redirect
+from app import app, db
+from forms import LoginForm, AddTask
+from models import User, Task
 
 
 
 @app.route('/')
-@app.route('/index', methods = ['GET', 'POST'])
+
+
+@app.route('/index', methods=['GET', 'POST'])
 def index():
     form = AddTask()
     user = { 'nickname': 'Dan'}  # fake user
-    tasks = {1: 'passport', 2: 'eat', 3: 'Amazing'}
+    #tasks = {1: 'passport', 2: 'eat', 3: 'Amazing'}
+    tasks = {}
+    all_tasks = Task.query.all()
+    i = 0
+    for ta in all_tasks:
+        new_dict = {i : ta.creator}
+        i += 1
+        tasks.update(new_dict)
+
+    if form.validate_on_submit():
+        session['new_task'] = [i, form.name.data]
+        #task = Task(number=i, creator=235026, time = 20)
+        #db.session.add(task)
+        #db.session.commit()
+        #return redirect('/index')
+        return redirect('/add_task')
     return render_template("index.html",
         title = 'Home',
         user = user,
         form = form,
         tasks = tasks)
+
+@app.route('/add_task', methods=['GET', 'POST'])
+def add_task():
+    try:
+        tasker = session['new_task']
+        t_number = tasker[0]
+        name = tasker[1]
+        task = Task(number= t_number, creator=235026, time = 20)
+    except KeyError:
+        task = Task(number= 10, creator=000000, time = 20)
+    db.session.add(task)
+    db.session.commit()
+    return redirect('/index')
