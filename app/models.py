89f63ac33052dba08d931d15150a35e9fa38b847
Andreas <andrea.varotsis@gmail.com>
2014-03-29 20:13:59 -0700

    fixing heroku bugs

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from app import db
 
 class User(db.Model):
-    id = db.Column(db.String, primary_key = True)
+    id = db.Column(db.String(6), primary_key = True)
     password = db.Column(db.String(10))
     tasks = db.relationship('Task', backref = 'creator', lazy = 'dynamic')
     role = db.Column(db.Integer)
@@ -26,7 +26,7 @@ class Task(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     type = db.Column(db.Integer)
     type_label = db.Column(db.String)
-    user_id = db.Column(db.String(120), db.ForeignKey('user.id'))
+    user_id = db.Column(db.String(6), db.ForeignKey('user.id'))
     start_weekday = db.Column(db.Integer)
     start_day = db.Column(db.Integer)
     start_month = db.Column(db.Integer)
